1a57aa738528200a0a5a391467154be3be730055
1F10240037 <s1f102400370@iniad.org>
2024-12-20 14:13:38 +0900

    top.htmlにURLを追加

--- volunteer/urls.py
+++ volunteer/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
 	path('', views.top, name='top'),
-	path('', views.organization_list, name='organization_list'),
+	path('organization', views.organization_list, name='organization_list'),
 ]
