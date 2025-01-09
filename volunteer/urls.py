a2d1cadd792c82b95dec421335d5946ef884a9bf
1f10240294 <s1f102402945@iniad.org>
2025-01-09 13:45:30 +0900

    設置

--- volunteer/urls.py
+++ volunteer/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-	path('', views.top, name='top'),
+	path('', views.index, name='index'),
 	path('organization', views.organization_list, name='organization_list'),
     path('about/', views.about, name='about'),
     path('contact/', views.contact, name='contact'),
--- volunteer/views.py
+++ volunteer/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
-def top(request):
-    return render(request, 'volunteer/top.html')
+def index(request):
+    return render(request, 'volunteer/index.html')
 
 def organization_list(request):
     return render(request, 'volunteer/organization_list.html')
